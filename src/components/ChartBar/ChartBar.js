59c2700d67bb34bc5d072213b3a3678398d10133
Dimterion <dimterion59@gmail.com>
2022-05-18 17:20:09 +0200

    Added styles for the Barchart

--- src/components/ChartBar/ChartBar.js
+++ src/components/ChartBar/ChartBar.js
@@ -1,11 +1,19 @@
-import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import "./chartBar.css";
 
 function ChartBar(props) {
   const numbers = [1, 2, 3, 4, 5, 6, 7];
 
   const data = props.activities.map((activity) => ({
-    number: numbers,
+    day: numbers,
     kilograms: activity.kilogram,
     calories: activity.calories / 7,
   }));
@@ -23,40 +31,73 @@ function ChartBar(props) {
     return null;
   };
 
+  const legendTextColor = (value) => {
+    return (
+      <span style={{ color: "var(--chartsSecondaryColor)" }}>{value}</span>
+    );
+  };
+
   return (
     <BarChart
       className="barChart-container"
-      width={600}
+      width={800}
       height={300}
       data={data}
-      barCategoryGap={50}
       barSize={10}
+      barGap={10}
       margin={{
-        top: 70,
-        right: 0,
+        top: 50,
+        right: 10,
         left: 40,
         bottom: 20,
       }}
     >
       <XAxis
-        dataKey="number"
-        tickSize={0}
-        padding={{ left: -30, right: -30 }}
+        dataKey="day"
+        tickSize={false}
+        padding={{ left: -40, right: -41 }}
+        dy={20}
       />
       <YAxis
         orientation="right"
         tickCount={3}
         padding={{ top: 10 }}
         axisLine={false}
-        tickSize={0}
+        tickLine={false}
+        dx={35}
+      />
+      <CartesianGrid
+        strokeDasharray="2"
+        vertical={false}
+        horizontalPoints={[-1, 157, 73]}
       />
       <Tooltip content={<DailyActivityTooltip />} />
-      <Bar radius={[10, 10, 0, 0]} dataKey="kilograms" />
-      <Bar radius={[10, 10, 0, 0]} dataKey="calories" fill="#ff0101" />
-      <text x="1%" y="10%">
-        Daily Activity
+      <Bar
+        name="Weight (kg)"
+        radius={[10, 10, 0, 0]}
+        dataKey="kilograms"
+        fill="var(--chartsSecondaryColor)"
+      />
+      <Bar
+        name="Burned calories (kCal)"
+        radius={[10, 10, 0, 0]}
+        dataKey="calories"
+        fill="var(--primaryColor)"
+      />
+      <text x="5%" y="15%" fill="var(--chartsSecondaryColor)" fontWeight={600}>
+        Daily activity
       </text>
-      <Legend verticalAlign="top" align="right" />
+      <Legend
+        verticalAlign="top"
+        align="right"
+        iconType="circle"
+        iconSize={7}
+        wrapperStyle={{
+          marginTop: -23,
+          marginRight: 10,
+        }}
+        formatter={legendTextColor}
+      />
     </BarChart>
   );
 }
